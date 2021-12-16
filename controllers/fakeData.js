a82336168065d1c9e70359d8073f0fc9e01d2a89
Dmytro_Yaichenia <dmytro.yaichenia@intellias.com>
2021-12-16 19:56:31 +0200

    feat: added express (HW4)

---
+++ controllers/fakeData.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    author: "John Dow",
+    title: "Natural language interface accessibility",
+    date: new Date().toDateString(),
+    likeNumber: 20,
+    isLiked: false,
+    content: "lorem",
+  },
+  {
+    author: "Anderson Bred",
+    title: "Accessibility of Remote Mettings",
+    date: new Date().toDateString(),
+    likeNumber: 2,
+    isLiked: true,
+    content: "lorem",
+  },
+];
